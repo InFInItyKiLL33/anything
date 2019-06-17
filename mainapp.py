eafa3f7300245ff5da2cb7887794ae9b8b7cd265
Infinity <demo@demos-MacBook-Pro.local>
2019-06-17 16:49:58 +0800

    formatting

--- mainapp.py
+++ mainapp.py
@@ -5,7 +5,7 @@ import os
 
 jinjaEnv = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
+    extensions=["jinja2.ext.autoescape"],
     autoescape=True)
 
 class WelcomeHandler(webapp2.RequestHandler):
