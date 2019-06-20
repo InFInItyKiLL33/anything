2cf74ce4f7bd76647a09070a7a8bc87cfb2e2a54
BuildTools <unconfigured@null.spigotmc.org>
2019-06-20 14:18:04 +0800

    finished main except for underline

--- mainapp.py
+++ mainapp.py
@@ -23,28 +23,30 @@ class MainHandler(webapp2.RequestHandler):
 
 class GenerateHandler(webapp2.RequestHandler):
     def get(self):
-        self.redirect("/main")
+        global namesDict
+        self.response.write(jinjaEnv.get_template("templates/generate.html").render(namesDict))
+        #self.redirect("/main")
         
     def post(self): ##change most "" to actual name of input
-        print("LUL")
         global namesDict
         i = 0
         while True: #get all the names and age input
-            if self.request.get(str(i)) == "":
-                print(self.request.get(str(i)))
+            try: 
+                self.request.get(str(i))
+            except KeyError:
                 if i == 0:
                     self.redirect("/main")
                     return
                 break
-            #00 -> name 1, 01 -> age 1, 10 -> name 2, 11 -> age 2, 20 -> name 3, 21 -> age 3, etc.
+            if self.request.get(str(i)) == "":
+                break
             namesDict.update({
-                str(i) + "0": self.request.get(str(i)).split(", ")[0],
-                str(i) + "1": self.request.get(str(i)).split(", ")[1]
+                "a" + str(i) + "0": self.request.get(str(i)).split(", ")[0],
+                "a" + str(i) + "1": self.request.get(str(i)).split(", ")[1]
             })
             i += 1
         namesDict.update({"filters": self.request.get("filters")})
-        print(namesDict)
-        self.response.write(jinjaEnv.get_template("templates/generate.html").render(namesDict))
+        self.response.write(jinjaEnv.get_template("templates/generate.html").render(namesDict)) #00 -> name 1, 01 -> age 1, 10 -> name 2, 11 -> age 2, 20 -> name 3, 21 -> age 3, etc. call filters for filters in binary
 
 class AboutUsHandler(webapp2.RequestHandler):
     def get(self):
